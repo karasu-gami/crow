822a436c60059dc278b1a45eb9a39ab08aaa89f0
the chosen one <pabloalbernazrincon@gmail.com>
2026-01-10 12:40:25 -0300

    Fix emojis

--- src/utils/Guild.ts
+++ src/utils/Guild.ts
@@ -11,9 +11,9 @@ export const Guild = {
     hs_brilliance: "<:hs_brilliance:1459567188917620880>",
     hs_bravery: "<:hs_bravery:1459567187336499220>",
     hs_balance: "<:hs_balance:1459567184622649577>",
-    hypesquad_badge: "<:hypesquad_badge:1459567183242723438>",
-    quest_badge: "<:quest_badge:1459567181905006745>",
-    nitro_badge: "<:opal_nitro_badge:1459567180541595862>",
+    hypesquad_badge: "<:hypesquad_badge:1459567180541595862>",
+    quest_badge: "<:quest_badge:1459567183242723438>",
+    nitro_badge: "<:opal_nitro_badge:1459567181905006745>",
     orbs_badge: "<:orbs_badge:1459567178381525083>",
   },
 };
